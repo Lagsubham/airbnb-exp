db8d4bc6342443275fb86258903597103d92ebbd
Your GitHub Username <subhammandal576@gmail.com>
2023-07-26 22:45:05 +0530

    one time push

---
+++ src/data.js
@@ -0,0 +1,105 @@
+export default [
+    {
+        id: 1,
+        title: "Life Lessons with Katie ",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "./images/swimar.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        openSpots: 0,
+        location: "Online",
+        
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "./images/wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/mountain-bike.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+
+    
+
+    {
+        id: 4,
+        title: "Group bodybuilder",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/bodybuilder.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "USA",
+        openSpots: 0,
+    },
+
+
+    {
+        id: 5,
+        title: "Group fine-lady",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/fine-lady.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "canada",
+        openSpots: 7,
+    },
+   
+
+    {
+        id: 6,
+        title: "Group underwater ",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/underwater.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "SEA",
+        openSpots: 66,
+    },
+  
+    {
+        id: 7,
+        title: "Group karate-kid",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/karate-kid.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "japan",
+        openSpots: 0,
+    },
+
+
+
+]
